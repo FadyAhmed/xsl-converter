7fdaca327e4a8846e62df7c91035cf38b8df715b
= <fatemagamal1999@gmail.com>
2023-01-10 13:23:06 +0200

    testing mapComments in main

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 #include "classes.cpp"
 #include <string>
 
+/*
 void mapComments(TreeNode* root) {
     // map each comment to node
     // delete isCommentFlag
@@ -15,6 +16,53 @@ void mapComments(TreeNode* root) {
         mapComments(&(root->children[i]));
     }
 }
+*/
+string commentNodeType(TreeNode* root)
+{    string type ="";
+    if (root->value == "Optional :")
+    {
+       type = "if";
+       return type;
+    }
+    if (root->value == "One or more time :")// le7ad ma net2aked bas
+    {
+       type = "for-each";
+       return type;
+    }
+}
+
+void convertToIf(TreeNode* root)
+{   string attribute = "test";
+    root->value="xsl:if";
+    root->keys.push_back(attribute);
+    root->values.push_back("path");
+}
+void convertToForEach(TreeNode* root)
+{   string attribute = "select";
+    root->value ="xsl:for-each";
+    root->keys.push_back(attribute);
+    root->values.push_back("path");
+}
+
+void mapComments(TreeNode* root) {
+    // map each comment to node
+    // delete isCommentFlag
+    if (root->isComment) {
+       // root->value = "was Comment";
+        string type =commentNodeType(root);
+        if(type == "if")
+        {convertToIf(root);}
+        else if(type== "for-each")
+        {convertToForEach(root);}
+        root->isComment = false;
+    }
+    for (int i = 0; i < root->children.size(); i++)
+    {
+        mapComments(&(root->children[i]));
+    }
+}
+
+
 
 
 
