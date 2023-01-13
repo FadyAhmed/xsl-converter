37ac85e4356a74852aa4427e414093e9b6bae6b2
= <fatemagamal1999@gmail.com>
2023-01-13 23:58:19 +0200

    organizing main , but not actually organized :")

--- main.cpp
+++ main.cpp
@@ -10,55 +10,42 @@
 
 using namespace std;
 
-vector<string> parsingCSV (string infile){
-     //csv file parsing
-  string Text="";
-  string txt="";
-  ifstream MyFile(infile);
-  while (getline (MyFile, Text)) {
-   txt += Text;}
-   MyFile.close();
-   std::stringstream str_strm(txt);
-   string tmp;
-   vector<string> words;
-   char delim = ',';
-   while (std::getline(str_strm, tmp, delim)) {
-      words.push_back(tmp);
-   }
-  //
-  // for(auto it = words.begin(); it != words.end(); it++) {
-     // std::cout << *it << std::endl;
-   //}
-
-  return words;
+ //Template for overloading insertion operator to print map
+template <typename key, typename value>
+ostream &operator<<(ostream &stream, const map<key, value> &myMap)
+{
+    for(const pair<key, value> &t: myMap)
+    {
+        stream << t.first<<" "<<t.second<<" "<<endl;
+    }
+    return stream;
 }
-
 
 
 int main()
 {
     ifstream MyReadFile("test.txt");
-
     string myText = "";
     string xmlText = "";
     // Use a while loop together with the getline() function to read the file line by line
     while (getline(MyReadFile, myText))
-    {
-        // Output the text from the file
+    {// Output the text from the file
         xmlText += myText;
     }
-
     // Close the file
     MyReadFile.close();
 
     auto words= parsingCSV("sample.csv");
-    cout<< words[0];
 
     ProcessedFile f = parseXml(xmlText);
     assignPaths(&f.tree.root);
     cout << "-------------------------------------\n";
-    printPaths(&f.tree.root);
-    mapComments(&f.tree.root);
+    //printPaths(&f.tree.root);
+    map<string, string> m;
+    m= mappingPaths(words);
+    cout << m;
+    mapComments(&f.tree.root,m);
+    mapText(&f.tree.root,m);
     f.print();
 
     return 0;
